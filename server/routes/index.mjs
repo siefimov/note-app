04e4465f8774490e94596806692b6471d440f2a2
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-10-27 16:25:44 +0300

    feat: Add Express routes and handlers for notes
    Created Express routes and request handlers for managing notes, including retrieving notes by notebookId, adding new notes, updating notes, and deleting notes.

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import notebookRouter from './notebookRouter.mjs';
+import noteRouter from './notesRouter.mjs';
 
 const router = new Router();
 
 router.use('/notebooks', notebookRouter);
+router.use('/notes', noteRouter);
 
-export default router
-
+export default router;
---
+++ server/routes/notesRouter.mjs
@@ -0,0 +1,70 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const router = express.Router();
+
+import { NoteModel } from '../models/notes.mjs';
+
+router.get('/:notebookId', async (req, res) => {
+    try {
+        const notebookId = req.params.notebookId;
+        const allNotes = await NoteModel.find({ notebookId });
+        res.status(200).json(allNotes);
+    } catch (error) {
+        res.status(500).json({ error: 'Error getting notes by notebook id' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newNote = new NoteModel({
+            title: req.body.title,
+            description: req.body.description,
+            notebookId: req.body.notebookId,
+            createdAt: req.body.createdAt,
+            updatedAt: req.body.updatedAt,
+        });
+
+        const saveNote = await newNote.save();
+        res.status(201).json(saveNote);
+    } catch (error) {
+        res.status(500).json('Error adding notes');
+    }
+});
+
+router.patch('/', async (req, res) => {
+    try {
+        const { _id, title, description } = req.body;
+
+        const updatedNote = await NoteModel.findOneAndUpdate({ _id }, { title }, { description }, { new: true });
+
+        if (!updatedNote) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating the note.' });
+    }
+});
+
+router.delete('/:_id', async (req, res) => {
+    try {
+        const noteId = req.params._id;
+
+        if (!noteId || !mongoose.isValidObjectId(noteId)) {
+            return res.status(400).json({ error: 'Not corrected _id for deleting note' });
+        }
+
+        const deletedNote = await NoteModel.findByIdAndDelete(noteId);
+
+        if (!deletedNote) {
+            return res.status(404).json({ error: 'Note was not find' });
+        }
+
+        res.status(200).json(deletedNote);
+    } catch (error) {
+        res.status(500).json({ error: 'Error deleting the note' });
+    }
+});
+
+export default router;

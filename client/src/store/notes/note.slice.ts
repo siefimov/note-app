7d6e28888352997739ec47e186b6a636248b02c8
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-10-28 18:22:59 +0300

    feat/front: Implement reducer logic to delete note
    This commit adds the reducer logic to handle the successful deletion of a note.
    When the `deleteNote` action is fulfilled, the reducer filters out the deleted note
    from the state's `list` and decrements the `count` by 1.
    This ensures that the state is updated correctly to reflect the removal of the note from the UI.

--- client/src/store/notes/note.slice.ts
+++ client/src/store/notes/note.slice.ts
@@ -1,37 +1,29 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
-import { Note } from './initialState';
-import { getNotes, addNote } from './note.actions';
+// import { Note } from './initialState';
+import { getNotes, addNote, deleteNote } from './note.actions';
 
 const notesSlice = createSlice({
     name: 'notes',
     initialState,
     reducers: {
-        // addNote(state, action: PayloadAction<Note>) {
-        //     state.list.push(action.payload);
-        //     state.count += 1;
+        // updateNote(state, action: PayloadAction<Partial<Note>>) {
+        //     const { id, title, description, updatedAt } = action.payload;
+
+        //     const noteToUpdate = state.list.find((note) => note.id === id);
+
+        //     if (noteToUpdate) {
+        //         if (title !== undefined) {
+        //             noteToUpdate.title = title;
+        //         }
+        //         if (description !== undefined) {
+        //             noteToUpdate.description = description;
+        //         }
+        //         if (updatedAt !== undefined) {
+        //             noteToUpdate.updatedAt = updatedAt;
+        //         }
+        //     }
         // },
-        deleteNote(state, action: PayloadAction<number | null>) {
-            state.list = state.list.filter((note) => note.id !== action.payload);
-            state.count -= 1;
-        },
-        updateNote(state, action: PayloadAction<Partial<Note>>) {
-            const { id, title, description, updatedAt } = action.payload;
-
-            const noteToUpdate = state.list.find((note) => note.id === id);
-
-            if (noteToUpdate) {
-                if (title !== undefined) {
-                    noteToUpdate.title = title;
-                }
-                if (description !== undefined) {
-                    noteToUpdate.description = description;
-                }
-                if (updatedAt !== undefined) {
-                    noteToUpdate.updatedAt = updatedAt;
-                }
-            }
-        },
     },
     extraReducers: (builder) => {
         builder.addCase(getNotes.fulfilled, (state, action) => {
@@ -41,9 +33,13 @@ const notesSlice = createSlice({
             state.list.push(action.payload);
             state.count += 1;
         });
+        builder.addCase(deleteNote.fulfilled, (state, action) => {
+            state.list = state.list.filter((note) => note._id !== action.payload);
+            state.count -= 1;
+        });
     },
 });
 
-export const { deleteNote, updateNote } = notesSlice.actions;
+// export const { updateNote } = notesSlice.actions;
 
 export default notesSlice.reducer;

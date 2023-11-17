f5173a380fac768705008a17184bb56f29a7f443
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-18 00:12:07 +0200

    front: refactored NoteList

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -1,51 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useAppSelector } from '../store/hooks';
 
-import Clock from '../helper/Clock';
 import { NoteItem } from './NoteItem/NoteItem';
 import { NoteTableView } from './NoteTableView/NoteTableView';
 import ToggleViewButton from './ToggleViewButton';
-
-import { AddNoteModal } from './NoteItem/components/AddNoteModal';
 
 export const NoteList: React.FC = () => {
     const notes = useAppSelector((state) => state.notes.data);
     const notebooks = useAppSelector((state) => state.notebooks.list);
     const activeNotebook = notebooks.filter((notebook) => notebook.isActive === true);
+
     const [isTableView, setIsTableView] = useState(false);
 
     const activeNotebookNotes = notes.filter((note) => note.notebookId === activeNotebook[0]?._id);
-    const [activeNotebookTitle, setActiveNotebookTitle] = useState('');
 
     const handleListClick = () => {
         setIsTableView(!isTableView);
     };
 
-    useEffect(() => {
-        setActiveNotebookTitle(activeNotebook[0]?.title);
-    }, [activeNotebook, notebooks, notes]);
-
     return (
         <div className=' basis-[80%] w-[100%] p-6'>
-            <Clock />
-            <div className='flex justify-between mb-6 border-b-2 pb-2'>
-                <div className='flex flex-col'>
-                    <h3 className='text-teal-600 font-bold uppercase transition-all duration-150'>
-                        <span className='lowercase text-slate-700 font-thin'>Selected Notebook: </span>
-                        {activeNotebook.length > 0 ? (
-                            activeNotebookTitle
-                        ) : (
-                            <span className='text-red-500 lowercase font-normal'>No selected Notebook</span>
-                        )}
-                    </h3>
-                    <p className='text-slate-700 font-thin'>
-                        notes in notebook: <span className='text-teal-600 font-bold'>{activeNotebookNotes.length}</span>
-                    </p>
-                </div>
-                <div>
-                    <AddNoteModal notebookId={activeNotebook[0]?._id} />
-                </div>
-            </div>
             <ToggleViewButton isTableView={isTableView} handleListClick={handleListClick} />
 
             {isTableView && <NoteTableView />}

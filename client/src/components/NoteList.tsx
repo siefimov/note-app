7d248b0991335c81d0c98641c692439e03a26d34
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-20 15:58:49 +0200

    front: added search notes logic

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -13,20 +13,39 @@ export const NoteList: React.FC = () => {
     const [isTableView, setIsTableView] = useState(false);
 
     const activeNotebookNotes = notes.filter((note) => note.notebookId === activeNotebook[0]?._id);
+    const [searchInputValue, setSearchInputValue] = useState('');
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInputValue(e.target.value.toLowerCase());
+    };
+
+    const displayedNotes = activeNotebookNotes.filter(
+        (note) =>
+            note.title.toLowerCase().includes(searchInputValue) ||
+            note.description.toLowerCase().includes(searchInputValue)
+    );
     const handleListClick = () => {
         setIsTableView(!isTableView);
     };
 
     return (
-        <div className=' basis-[80%] w-[100%] p-6'>
-            <ToggleViewButton isTableView={isTableView} handleListClick={handleListClick} />
+        <div className=' basis-[80%] w-[100%] px-6 pb-6'>
+            <div className='flex justify-end items-center mb-6'>
+                <input
+                    type='search'
+                    value={searchInputValue}
+                    onChange={(e) => handleChange(e)}
+                    placeholder='search'
+                    className='p-2 rounded-xl mr-4 bg-slate-50'
+                />
+                <ToggleViewButton isTableView={isTableView} handleListClick={handleListClick} />
+            </div>
 
             {isTableView && <NoteTableView />}
 
             <div className='flex gap-3 flex-wrap transition-all duration-500'>
                 {!isTableView &&
-                    activeNotebookNotes.map((note) => (
+                    displayedNotes.map((note) => (
                         <NoteItem
                             key={note._id}
                             title={note.title}
